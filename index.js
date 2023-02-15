c91297d56cd6a6ad434f4a03fba07c02130b16be
bpat714 <38747233+bpat714@users.noreply.github.com>
2023-02-15 17:22:33 +1300

    retrieves list of products given skincare step

---
+++ index.js
@@ -0,0 +1,34 @@
+exports.handler = async (event) => {
+    try {
+      const { Configuration, OpenAIApi } = require("openai");
+  
+      const configuration = new Configuration({
+        apiKey: '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',
+      });
+  
+      const openai = new OpenAIApi(configuration);
+  
+      const prompt = `list of recommended products for` + event.step;
+  
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response.data.choices[0].text),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: error.message }),
+      };
+    }
+  };
+  
